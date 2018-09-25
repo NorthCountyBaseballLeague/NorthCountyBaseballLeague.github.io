54fea66361bffc834fc3479785177b2cfd21e37a
HUNTER\EMagallanes <sdncbaseball@gmail.com>
2018-09-25 13:07:05 -0700

    Moved all the javascript to for the Standings file to another file;

---
+++ Standings.js
@@ -0,0 +1,35 @@
+var calculateAllWinPercentages = function () {
+    var winsEls = document.getElementsByClassName("wins");
+    var lossesEls = document.getElementsByClassName("losses");
+    var winPctsEls = document.getElementsByClassName("win-pct");
+
+    for (var i = 0; i < winsEls.length; i++) {
+        var wins = parseInt(winsEls[i].innerHTML);
+        var losses = parseInt(lossesEls[i].innerHTML);
+
+        winPctsEls[i].innerHTML = calculateIndividualWinPercentage(wins, losses);
+    }
+};
+
+var calculateIndividualWinPercentage = function (wins, losses) {
+    var winPct = wins / (wins + losses);
+    var roundedWinPct = Math.round(winPct * 1000) / 1000;
+
+    return formatWinPercentage(roundedWinPct);
+};
+
+var formatWinPercentage = function (winPct) {
+    var winPctStr = winPct.toString();
+
+    if (winPctStr.length === 1) {
+        winPctStr = winPctStr + ".";
+    }
+
+    while (winPctStr.length < 5) {
+        winPctStr = winPctStr + "0";
+    }
+
+    return winPctStr;
+};
+
+calculateAllWinPercentages();
