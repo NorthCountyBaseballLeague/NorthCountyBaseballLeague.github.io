d63674c7bc71cf2556fbe7eeadbbfe30aa857309
HUNTER\EMagallanes <sdncbaseball@gmail.com>
2018-09-26 16:48:38 -0700

    Moved the index.html file out of the HTML folder so we can view the webpage; Updated the HTML files so they reference the correct index.html; Added functions to calculate the number of games behind; TODO: Add tests for calculating number of games behind;

--- js/Standings.js
+++ js/Standings.js
@@ -32,7 +32,36 @@ function formatWinPercentage (winPct) {
     return winPctStr;
 };
 
-// TODO: Add functions to calculate the number of games behind
+function calculateAllGamesBehind (winsArray, lossesArray, gamesBehindArray) {
+    var wins = [];
+    var losses = [];
+
+    for (var i = 0; i < winsArray.length; i++) {
+        wins.push(parseInt(winsArray[i].innerHTML));
+        losses.push(parseInt(lossesArray[i].innerHTML));
+
+        var winDiff = wins[0] - wins[i];
+        var lossDiff = losses[i] - losses[0];
+        var gb = (winDiff + lossDiff) / 2;
+
+        gamesBehindArray[i].innerHTML = formatGamesBehind(gb);
+    }
+
+};
+
+function formatGamesBehind (gamesBehind) {
+    if (gamesBehind === 0) {
+        return "-";
+    }
+    
+    var gamesBehindStr = gamesBehind.toString();
+    if (!gamesBehindStr.includes(".")) {
+        gamesBehindStr += ".0";
+    }
+
+    return gamesBehindStr;
+};
+
 // TODO: Add functions to calculate the wins and losses
 // TODO: Add functions to calculate the last 10 games record
 // TODO: Add functions to calculate the win/loss streak
@@ -40,5 +69,7 @@ function formatWinPercentage (winPct) {
 module.exports = {
     calculateAllWinPercentages,
     calculateIndividualWinPercentage,
-    formatWinPercentage
+    formatWinPercentage,
+    calculateAllGamesBehind,
+    formatGamesBehind
 }
