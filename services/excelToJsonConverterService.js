357ee26e65e00a86beaa1432a90047712bac3ee8
North County Baseball League <sdncbaseball@gmail.com>
2019-04-22 19:35:28 -0700

    Added service to convert statistics excel to json object;
    Added node dependencies and script to build;

---
+++ services/excelToJsonConverterService.js
@@ -0,0 +1,37 @@
+'use strict';
+const excelToJson = require('convert-excel-to-json');
+const fs = require('fs');
+
+function convertToJson () {
+    return excelToJson({
+        sourceFile: 'filebase/NCBL Statistics.xlsx',
+        header: {
+            rows: 1
+        },
+        columnToKey: {
+            '*': '{{columnHeader}}'
+        }
+    });
+}
+
+function generateFileString(jsonString) {
+    const declarationString = 'const stats = ';
+    let jsFileString = declarationString + jsonString + ';';
+    const exportString = '\n\nmodule.exports = stats;';
+    jsFileString += exportString;
+    return jsFileString;
+}
+
+function writeToFile () {
+    const jsonObject = convertToJson();
+    const jsonString = JSON.stringify(jsonObject);
+    const jsFileString = generateFileString(jsonString);
+    const statsFile = 'filebase/statisticsFilebase.js';
+    fs.writeFile(statsFile, jsFileString, 'utf8', function(err) {
+        if (err) {
+            console.log('There was an error: ', err);
+        }
+    });
+}
+
+module.exports = writeToFile();
