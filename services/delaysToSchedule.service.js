3d3090c7128b1c5c75c23f410303b1b845c56e01
NCBL <sdncbaseball@gmail.com>
2019-12-10 12:46:48 -0800

    Adding delay to 12/8/19

--- services/delaysToSchedule.service.js
+++ services/delaysToSchedule.service.js
@@ -84,6 +84,7 @@ function delaysToScheduleService() {
         addDelaysToWeeks(schedulesWithTeams, '2019spring', '5/26/2019');
         addDelaysToWeeks(schedulesWithTeams, '2019spring', '7/28/2019');
         addDelaysToWeeks(schedulesWithTeams, '2019spring', '9/1/2019');
+        addDelaysToWeeks(schedulesWithTeams, '2019fall', '12/8/2019');
     }
 
     return {
